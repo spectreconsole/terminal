28a726acb279d46901c4e1067465c33f3ba5dec8
Patrik Svensson <patrik@patriksvensson.se>
2021-06-07 18:32:29 +0200

    Add Linux terminal support.

--- src/Spectre.Terminals/TerminalFactory.cs
+++ src/Spectre.Terminals/TerminalFactory.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.InteropServices;
+using Spectre.Terminals.Drivers;
 using Spectre.Terminals.Windows;
 
 namespace Spectre.Terminals
@@ -10,7 +11,11 @@ namespace Spectre.Terminals
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                return new Terminal(new WindowsDriver(emulate: false));
+                return new Terminal(new WindowsDriver());
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return new Terminal(new LinuxDriver());
             }
 
             throw new PlatformNotSupportedException();
